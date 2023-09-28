998bfb9837c1a05db51df1a762b862bb724d9f50
el mehdi <elmehdibennanikhir@gmail.com>
2023-09-28 21:53:26 +0100

    create functions to display X and O when user clicks

--- js/script.js
+++ js/script.js
@@ -1,11 +1,38 @@
 let cells=["","","","","","","","",""]
 const border = document.querySelector('.border')
-border.innerHTML="";
-for (let i = 0; i < cells.length; i++) {
-   const cell =document.createElement('div')
-   cell.className='bordChild'
-   cell.innerText=cells[i]
-   border.appendChild(cell)
- 
-}
+let player1='X'
+let player2='O'
+let turn=true
+
+function play(index){
+    if(cells[index] !=='') return;
+    if(turn && cells[index]==''){
+        cells[index]=player1
+        renderBorder()
+    }
+   turn=!turn
    
+   console.log(turn);
+    
+   if(turn&&cells[index]==''){
+             cells[index]=player2
+             renderBorder()
+    }
+
+
+
+
+}
+
+function renderBorder(){
+    border.innerHTML="";
+    for (let i = 0; i < cells.length; i++) {
+       const cell =document.createElement('div')
+       cell.className='bordChild'
+       cell.innerText=cells[i]
+       cell.addEventListener('click',()=>{play(i)})
+       border.appendChild(cell) 
+    }
+       
+}
+renderBorder()
